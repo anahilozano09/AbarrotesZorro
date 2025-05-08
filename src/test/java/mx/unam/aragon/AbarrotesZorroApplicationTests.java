cf20a830d9bef445b15f43e194368870fcbbd6b3
anahilozano09 <anahilozano319@aragon.unam.mx>
2025-05-08 16:38:18 -0600

    Proyecto Spring

---
+++ src/test/java/mx/unam/aragon/AbarrotesZorroApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.unam.aragon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbarrotesZorroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
