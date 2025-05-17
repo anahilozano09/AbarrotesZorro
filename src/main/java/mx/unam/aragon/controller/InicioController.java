c1ad106b186c4ab9a74fc8ba4dd5fd9f571b550c
DiegoArreola2003 <diegoarreola88.aragon.unam.mx>
2025-05-16 23:11:23 -0600

    Inicio de las vistas

--- src/main/java/mx/unam/aragon/controller/InicioController.java
+++ src/main/java/mx/unam/aragon/controller/InicioController.java
@@ -1,22 +1,36 @@
 package mx.unam.aragon.controller;
 
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-
 public class InicioController {
 
     @GetMapping("/")
     public String inicio(
             @RequestParam(value = "error", required = false) String error,
             @RequestParam(value = "logout", required = false) String logout,
-            Model model) {
+            Model model,
+            Authentication authentication) {
 
+        if (authentication != null && authentication.isAuthenticated()) {
+            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+            for (GrantedAuthority authority : authorities) {
+                if (authority.getAuthority().equals("ROLE_ADMIN")) {
+                    return "paginas/admin/inicio"; // Vista exclusiva del administrador
+                } else if (authority.getAuthority().equals("ROLE_CAJERO")) {
+                    return "paginas/cajero/inicio"; // Vista exclusiva del cajero
+                }
+            }
+        }
+
+        // Por defecto si no hay rol o no está autenticado
         model.addAttribute("contenido", "Bienvenido a Abarrotes el Zorro");
         return "inicio";
     }
@@ -30,5 +44,4 @@ public class InicioController {
     public String dashboard() {
         return "dashboard";
     }
-
 }
